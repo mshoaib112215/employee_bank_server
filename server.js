1851deaf4e1cfd3312d6f48ac9a9fdd03ce8a95c
mshoaib112215 <mshoaib112215@gmail.com>
2023-10-18 10:26:01 +0500

    ready to deploy on vercel

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const https = require("https"); // Import the 'https' module
+const http = require("http"); // Import the 'https' module
 const fs = require("fs");
 const dbConnect = require("./database/index");
 const { PORT } = require("./config/index");
@@ -8,16 +8,16 @@ const errorHandler = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const corsOptions = {
-  credentials: true,
-  origin: ["http://localhost:3000", "http://127.0.0.1:5173", "http://localhost:5173", "https://employee-bank.netlify.app"],
-};
+// const corsOptions = {
+//   credentials: true,
+//   origin: ["http://localhost:3000", "http://127.0.0.1:5173", "http://localhost:5173", "https://employee-bank.netlify.app"],
+// };
 
 const app = express();
 
 app.use(cookieParser());
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "50mb" }));
 
@@ -30,17 +30,17 @@ app.use("/storage", express.static("storage"));
 app.use(errorHandler);
 
 // Read SSL/TLS certificate and private key files
-const privateKey = fs.readFileSync("./cert/key.pem", "utf8");
-const certificate = fs.readFileSync("./cert/cert.pem", "utf8");
-const ca = fs.readFileSync("./cert/csr.pem", "utf8"); // Optional: Certificate Authority (CA) bundle
+// const privateKey = fs.readFileSync("./cert/key.pem", "utf8");
+// const certificate = fs.readFileSync("./cert/cert.pem", "utf8");
+// const ca = fs.readFileSync("./cert/csr.pem", "utf8"); // Optional: Certificate Authority (CA) bundle
 
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-};
+// const credentials = {
+//   key: privateKey,
+//   cert: certificate,
+//   ca: ca,
+// };
 
 // Create HTTPS server
-const httpsServer = https.createServer(credentials, app);
+const httpsServer = http.createServer( app);
 
 httpsServer.listen(PORT, console.log(`Backend is running on port: ${PORT} using HTTPS`));
